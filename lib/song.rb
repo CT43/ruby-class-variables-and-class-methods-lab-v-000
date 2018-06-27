dbd7a9be02221795655e1034af3ac9008c69dcaa
Christian Thomas <christian.thomas.2894@gmail.com>
2018-06-27 00:12:55 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -12,12 +12,17 @@ class Song
     @artist = artist
     @genre = genre
     @@count += 1
+    @@genres << genre
+    @@artists << artist 
   end
 
   def self.count
     @@count
   end
 
+  def self.artists 
+    
+  end
 
 
 end
